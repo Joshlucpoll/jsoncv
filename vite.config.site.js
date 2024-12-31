acc86f187289c499c84e8df1ebcd326fa47c1048
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2024-12-31 17:52:59 +0100

    updated config for self deployment

--- vite.config.site.js
+++ vite.config.site.js
@@ -1,60 +1,58 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
-import { TransformEjs } from './src/lib/vite-plugins';
+import { TransformEjs } from "./src/lib/vite-plugins";
 
-const rootDir = resolve(__dirname, 'src')
+const rootDir = resolve(__dirname, "src");
 const renderData = {
   meta: {
     title: "jsoncv",
-    description: "A toolkit for building your CV with JSON and creating stylish HTML/PDF files.",
-    url: "https://jsoncv.reorx.com",
+    description:
+      "A toolkit for building your CV with JSON and creating stylish HTML/PDF files.",
+    url: "https://jsoncv.joshlucpoll.dev",
     twitter: {
       card: "summary",
       username: "novoreorx",
-    }
+    },
   },
   editorMeta: {
     title: "jsoncv Editor",
     description: "The online editor of the jsoncv project.",
-    url: "https://jsoncv.reorx.com/editor/",
+    url: "https://jsoncv.joshlucpoll.dev/editor/",
     twitter: {
       card: "summary",
       username: "novoreorx",
-    }
-  }
-}
+    },
+  },
+};
 
 export default defineConfig({
-  root: 'src',
+  root: "src",
   build: {
     // allows 'import.meta.glob' to work
-    target: 'esnext',
+    target: "esnext",
     rollupOptions: {
       input: {
-        main: resolve(rootDir, 'index.html'),
-        editor: resolve(rootDir, 'editor/index.html'),
-        preview: resolve(rootDir, 'preview/index.html'),
+        main: resolve(rootDir, "index.html"),
+        editor: resolve(rootDir, "editor/index.html"),
+        preview: resolve(rootDir, "preview/index.html"),
       },
     },
   },
   resolve: {
     alias: {
       // remove the "Module "fs" has been externalized" warning for ejs
-      'fs': 'src/lib/fs-polyfill.js',
+      fs: "src/lib/fs-polyfill.js",
     },
   },
   plugins: [
     TransformEjs(),
-    ViteEjsPlugin(
-      renderData,
-      {
-        ejs: (viteConfig) => ({
-          // ejs options goes here.
-          views: [resolve(__dirname)],
-        })
-      }
-    ),
+    ViteEjsPlugin(renderData, {
+      ejs: (viteConfig) => ({
+        // ejs options goes here.
+        views: [resolve(__dirname)],
+      }),
+    }),
   ],
-})
+});
